66bdb9bc7ffdf8fa68cee22ed3730d484c84663f
João Henriques <joaoh88@gmail.com>
2014-06-06 02:22:14 +0100

    fixed bug with some \\CHARs

---
+++ compPCRE/src/org/comp/pcre/main/EntryPoint.java
@@ -0,0 +1,28 @@
+package org.comp.pcre.main;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.comp.pcre.grammar.PCRE;
+import org.comp.pcre.grammar.ParseException;
+import org.comp.pcre.grammar.SimpleNode;
+
+public class EntryPoint {
+
+	public static void main( String[] args ) throws ParseException
+	{ 
+		InputStream is = null;
+		try {
+			is = new ByteArrayInputStream( "c:\\windows\n".getBytes( "UTF-8" ) );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		  PCRE parser = new PCRE( is );//System.in ) ;
+		  
+		  SimpleNode val = parser.Start() ;
+		  System.out.println("Val:"+val.toString());
+		  val.dump("");
+		
+		
+	}
+}
